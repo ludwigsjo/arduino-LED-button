f9d5526d0ca4873a56d6b041a4d0ff054f368428
Ludwig Sjöberg <ludwig.sjoberg@skola.botkyrka.se>
2020-09-11 14:13:38 +0200

    När man klickade på en knapp så började LED lampa att lyssa genom arduinon

---
+++ src/main.cpp
@@ -0,0 +1,22 @@
+#include <Arduino.h>
+const int buttonPin = 2;     // the number of the pushbutton pin
+const int ledPin =  8;
+
+int buttonState = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  pinMode(buttonPin, INPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  buttonState = digitalRead(buttonPin);
+
+  if (buttonState == HIGH) {
+    digitalWrite(ledPin, HIGH);
+  } else {
+    digitalWrite(ledPin, LOW);
+  }
+}
